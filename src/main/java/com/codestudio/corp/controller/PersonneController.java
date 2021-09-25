148ad260735005fd9bf77f9710a8356954f129a2
francis-siva <fsivasithamparam@gmail.com>
2021-09-25 18:41:37 +0200

    Get Attribute from welcome.jsp fields

--- src/main/java/com/codestudio/corp/controller/PersonneController.java
+++ src/main/java/com/codestudio/corp/controller/PersonneController.java
@@ -1,7 +1,11 @@
 package com.codestudio.corp.controller;
 
+import java.util.Date;
+
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 public class PersonneController {
@@ -13,7 +17,11 @@ public class PersonneController {
 	
 	
 	@PostMapping("/register")
-	public void register() {
+	public void register(Model model) {
+		String prenom = (String) model.getAttribute("prenom");
+		String nom = (String) model.getAttribute("nom");
+		Date birth = (Date) model.getAttribute("birth");
 		
+		//Create person by autowired object instead of Person's instance
 	}
 }
