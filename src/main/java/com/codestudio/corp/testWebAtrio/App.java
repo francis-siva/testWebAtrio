97eb2842681b1594dc339b2223c0c9581b35cacd
francis-siva <fsivasithamparam@gmail.com>
2021-10-02 22:59:33 +0200

    Config Importante pour le Scan (JPA, Service, Controleur)

--- src/main/java/com/codestudio/corp/testWebAtrio/App.java
+++ src/main/java/com/codestudio/corp/testWebAtrio/App.java
@@ -3,9 +3,13 @@ package com.codestudio.corp.testWebAtrio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.codestudio.corp.controller")
+@SpringBootApplication(scanBasePackages = "com.codestudio.corp")
 @EntityScan("com.codestudio.corp.model")
+/* JpaRepository Explicit definition when its package location
+is different to base package */
+@EnableJpaRepositories("com.codestudio.corp.repository")
 public class App 
 {	
     public static void main( String[] args )
